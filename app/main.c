6b82d456f36fd2d08cbf25a1019852e37dc75666
xiaotian <canxuetian@gmail.com>
2022-12-05 01:47:11 +0800

    1-TIM+DAC+DMA播放8bit音乐-update-不使用BUFFER

--- app/main.c
+++ app/main.c
@@ -17,8 +17,7 @@ volatile uint8_t is_dac_dma_working_flag = 0;
 DAC_InitTypeDef DAC_InitStructure;
 DMA_InitTypeDef DMA_InitStructure;
 
-void HAL_DAC_Start_DMA_init(void)
-{
+void DAC_Start_DMA_init(void) {
     DAC_InitStructure.DAC_Trigger = DAC_Trigger_T6_TRGO;
     DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
     DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Disable;
@@ -27,7 +26,6 @@ void HAL_DAC_Start_DMA_init(void)
     /* DMA1_Stream6 channel7 configuration **************************************/
     DMA_DeInit(DMA1_Stream6);
     DMA_InitStructure.DMA_Channel = DMA_Channel_7;
-//BeepData[BEEP_DATA_LEN]
     DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t) &DAC->DHR8R2; //8位，右对齐
     DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
     DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
@@ -42,12 +40,12 @@ void HAL_DAC_Start_DMA_init(void)
     DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
 
     NVIC_InitTypeDef NVIC_InitStruct =
-    {
-        .NVIC_IRQChannel = DMA1_Stream6_IRQn,
-        .NVIC_IRQChannelCmd = ENABLE,
-        .NVIC_IRQChannelPreemptionPriority = 1,
-        .NVIC_IRQChannelSubPriority = 1
-    };
+            {
+                    .NVIC_IRQChannel = DMA1_Stream6_IRQn,
+                    .NVIC_IRQChannelCmd = ENABLE,
+                    .NVIC_IRQChannelPreemptionPriority = 1,
+                    .NVIC_IRQChannelSubPriority = 1
+            };
     NVIC_Init(&NVIC_InitStruct);
     DMA_ITConfig(DMA1_Stream6, DMA_IT_TC, ENABLE);
 
@@ -59,9 +57,15 @@ void HAL_DAC_Start_DMA_init(void)
     DAC_DMACmd(DAC_Channel_2, ENABLE);
 }
 
-void HAL_DAC_Start_DMA(void *dac_handle, void *dac_channel_1_or_2, uint8_t *Memory0BaseAddr, uint32_t BUFFER_SIZE,
-                       void *DAC_ALIGN_8B_R)
-{
+/**
+ * 因为DMA_SxNDTR寄存器只有16位，所以如果需要传的数据量大于16位，需要分包传输
+ * @param dac_handle
+ * @param dac_channel_1_or_2
+ * @param Memory0BaseAddr
+ * @param BUFFER_SIZE
+ * @param DAC_ALIGN_8B_R
+ */
+void DAC_Start_DMA(const uint8_t *Memory0BaseAddr, uint32_t BUFFER_SIZE) {
 
     /* Enable DMA1_Stream6 */
     DMA_Cmd(DMA1_Stream6, DISABLE);
@@ -72,47 +76,40 @@ void HAL_DAC_Start_DMA(void *dac_handle, void *dac_channel_1_or_2, uint8_t *Memo
 
     /* Enable DMA1_Stream6 */
     DMA_Cmd(DMA1_Stream6, ENABLE);
-
-
     is_dac_dma_working_flag = 1;
 }
 
-void DMA1_Stream6_IRQHandler()
-{
-    if (DMA_GetITStatus(DMA1_Stream6, DMA_IT_TCIF6) != 0)
-    {
+void DMA1_Stream6_IRQHandler() {
+    if (DMA_GetITStatus(DMA1_Stream6, DMA_IT_TCIF6) != 0) {
         DMA_ClearITPendingBit(DMA1_Stream6, DMA_IT_TCIF6);
         is_dac_dma_working_flag = 0;
     }
 }
 
+
 #define        BUFFER_SIZE        (1024)
-
-uint8_t Buffer0[BUFFER_SIZE] = {0};
-
 
 /**
  * 播放一段wav音乐的程序，程序上电后，只执行一次，把所有音乐播放完
  */
-void Music_Player(void)
-{
+void Music_Player(void) {
     uint32_t DataLength = 0;
     uint8_t *DataAddress = NULL;
 
     DataLength = sizeof(data) - 0x2c;
     DataAddress = (unsigned char *) (data + 0x2c);
     TIM6_Config();
-    HAL_DAC_Start_DMA_init();
+    DAC_Start_DMA_init();
 
-    while (DataLength >= BUFFER_SIZE)
-    {
-        memcpy(Buffer0, DataAddress, BUFFER_SIZE);
+    //每次DMA传输BUFFER_SIZE个字节
+    while (DataLength >= BUFFER_SIZE) {
         DataLength -= BUFFER_SIZE;
         DataAddress += BUFFER_SIZE;
-
-        HAL_DAC_Start_DMA(0, 0,
-                          (uint8_t *) Buffer0, BUFFER_SIZE, 0);
-
+        DAC_Start_DMA((uint8_t *) DataAddress, BUFFER_SIZE);
+        while (is_dac_dma_working_flag != 0);
+    }
+    if (DataLength > 0) {
+        DAC_Start_DMA((uint8_t *) DataAddress, DataLength);
         while (is_dac_dma_working_flag != 0);
     }
 
@@ -128,8 +125,7 @@ void Music_Player(void)
   * @param  None
   * @retval None
   */
-static void TIM6_Config(void)
-{
+static void TIM6_Config(void) {
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
     /* TIM6 Periph clock enable */
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
@@ -167,8 +163,7 @@ static void TIM6_Config(void)
 
 RCC_ClocksTypeDef RCC_Clocks;
 
-int main(void)
-{
+int main(void) {
     //4位抢占优先级，0位相应优先级
     __NVIC_SetPriorityGrouping(NVIC_PriorityGroup_4);
     /* SysTick end of count event each ms */
@@ -197,8 +192,7 @@ int main(void)
 
     Music_Player();
 
-    while (1)
-    {
+    while (1) {
         Delay(8000);
     }
 
